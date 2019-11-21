df33479a5771c4eedf3d049e8534f2bffcbd6607
jwelchman <j.welchman@yahoo.com>
2019-11-21 09:11:54 -0700

    easiest. challenge. ever. :P

--- answer.js
+++ answer.js
@@ -1,1 +1,17 @@
 // *** YOUR ANSWER BELOW ***
+
+let input = "Cold";
+
+function negate(input) {
+    return "un" + input;
+}
+
+function intensify(input) {
+    return "plus" + input;
+}
+
+function reinforce(input) {
+    return "double" + input;
+}
+
+console.log(reinforce(intensify(negate("proto"))));
